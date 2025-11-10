57ba10f438b63775166a1e925dfa245aa2c571d0
neworderofjamie <neworderofjamie@gmail.com>
2025-11-10 12:24:39 +0000

    switched to rescaled parameter set and added integer model to benchmark_lif

--- models/benchmark_lif/model.cc
+++ models/benchmark_lif/model.cc
@@ -42,6 +42,49 @@ public:
 };
 IMPLEMENT_SNIPPET(LIFHalf);
 
+class LIFInt : public NeuronModels::Base
+{
+public:
+    DECLARE_SNIPPET(LIFInt);
+
+    SET_SIM_CODE(
+        "scalar VFloat = (V / 32767.0);\n"
+        "if (RefracTime <= 0) {\n"
+        "  scalar alpha = ((Isyn + Ioffset) * Rmembrane) + Vrest;\n"
+        "  VFloat = alpha - (ExpTC * (alpha - VFloat));\n"
+        "}\n"
+        "else {\n"
+        "  RefracTime--;\n"
+        "}\n"
+        "V = max(-32768, min(32767, (int)round(VFloat * 32767.0)));\n"
+    );
+
+    SET_THRESHOLD_CONDITION_CODE("RefracTime < 0 && VFloat >= Vthresh");
+
+    SET_RESET_CODE(
+        "V = Vreset;\n"
+        "RefracTime = TauRefracTimestep;\n");
+
+    SET_PARAMS({
+        "C",          // Membrane capacitance
+        "TauM",       // Membrane time constant [ms]
+        "Vrest",      // Resting membrane potential [mV]
+        "Vreset",     // Reset voltage [mV]
+        "Vthresh",    // Spiking threshold [mV]
+        "Ioffset",    // Offset current
+        "TauRefrac"});
+
+    SET_DERIVED_PARAMS({
+        {"ExpTC", [](const ParamValues &pars, double dt){ return std::exp(-dt / pars.at("TauM").cast<double>()); }},
+        {"Rmembrane", [](const ParamValues &pars, double){ return  pars.at("TauM").cast<double>() / pars.at("C").cast<double>(); }},
+        {"TauRefracTimestep", [](const ParamValues &pars, double dt){ return (int)std::round(pars.at("TauRefrac").cast<double>() / dt); }}});
+
+    SET_VARS({{"V", "int16_t"}, {"RefracTime", "int16_t"}});
+
+    SET_NEEDS_AUTO_REFRACTORY(false);
+};
+IMPLEMENT_SNIPPET(LIFInt);
+
 void modelDefinition(ModelSpec &model)
 {
     model.setDT(1.0);
@@ -56,21 +99,21 @@ void modelDefinition(ModelSpec &model)
     //---------------------------------------------------------------------------
     // LIF model parameters
     ParamValues lifParams{
-        {"C", 1.0},
-        {"TauM", 20.0},
-        {"Vrest", -49.0},
-        {"Vreset", -60.0},
-        {"Vthresh", -50.0},
+        {"C", 3.75},
+        {"TauM", 10.0},
+        {"Vrest", 0.0},
+        {"Vreset", 0.0},
+        {"Vthresh", 1.0},
         {"Ioffset", 0.0},
-        {"TauRefrac", 5.0}};
+        {"TauRefrac", 2.0}};
 
     // LIF initial conditions
     VarValues lifInit{
-        {"V", -60.0},
+        {"V", 0.0},
         {"RefracTime", 0.0}};
 
     // Create IF_curr neuron
-    auto *pop = model.addNeuronPopulation<NeuronModels::LIF>("Pop", 10000, lifParams, lifInit);
+    auto *pop = model.addNeuronPopulation<NeuronModels::LIF>("Pop", 100000, lifParams, lifInit);
 
     // Enable spike recording
     pop->setSpikeRecordingEnabled(true);
