e7147c6b8c6d79825ff28e575f6017c8bcc9ae4c
Jamie Knight <neworderofjamie@gmail.com>
2026-01-21 15:49:29 +0000

    jammed in some example failure modes into single neuron figure

--- scripts/plot_spike_lag.py
+++ scripts/plot_spike_lag.py
@@ -56,7 +56,7 @@ def plot_voltage(axis, time: float, neuron: int = 0):
     t = np.arange(0, v_float.shape[0] * 0.1, 0.1)
     float_actor = axis.plot(t, v_float[:,neuron], linestyle="--", color="gray")[0]
     half_actor = axis.plot(t, v_half[:,neuron], alpha=0.5)[0]
-    half_rescale_actor = axis.plot(t, (v_half_rescale[:,neuron] * 15.0) - 65.0, 
+    half_rescale_actor = axis.plot(t, (np.float64(v_half_rescale[:,neuron]) * 15.0) - 65.0, 
                                    alpha=0.5)[0]
     axis.xaxis.grid(False)
     sns.despine(ax=axis)
@@ -74,8 +74,7 @@ def plot_rmse(axis, time: float, half_colour, half_rescale_colour, num_neurons: 
     half_rmse = []
     half_rescale_rmse = []
     for i in range(num_neurons):
-        v_half_rescale_scale = (v_half_rescale[:,i] * 15.0) - 65.0
-
+        v_half_rescale_scale = (np.float64(v_half_rescale[:,i]) * 15.0) - 65.0
         half_rmse.append(np.sqrt(np.sum(np.square(v_half[:,i] - v_float[:,i])) / v_float.shape[0]))
         half_rescale_rmse.append(np.sqrt(np.sum(np.square(v_half_rescale_scale - v_float[:,i])) / v_float.shape[0]))
 
@@ -88,17 +87,57 @@ def plot_rmse(axis, time: float, half_colour, half_rescale_colour, num_neurons: 
     axis.xaxis.grid(False)
     sns.despine(ax=axis)
 
+def plot_divergence(spike_axis, initial_axis, time: float, neuron: int, 
+                    half_colour, half_rescale_colour):
+    v_float = np.load(os.path.join("compare_neurons", f"v_float_0.1_{time}.npy"))
+    v_half = np.load(os.path.join("compare_neurons", f"v_half_0.1_{time}.npy"))
+    v_half_rescale = np.load(os.path.join("compare_neurons", f"v_half_rescale_0.1_{time}.npy"))
+
+    assert v_float.shape == v_half.shape
+    assert v_float.shape == v_half_rescale.shape
+    
+    t = np.arange(0, v_float.shape[0] * 0.1, 0.1)
+    for i, axis in enumerate([spike_axis, initial_axis]):
+        axis.plot(t, v_float[:,neuron], linestyle="--", color="gray")
+        axis.plot(t, v_half[:,neuron], alpha=0.5, color=half_colour, 
+                  marker="None" if i == 0 else ".")
+        axis.plot(t, (np.float64(v_half_rescale[:,neuron]) * 15.0) - 65.0, 
+                  alpha=0.5, color=half_rescale_colour)
+        axis.xaxis.grid(False)
+        axis.yaxis.grid(False)
+        sns.despine(ax=axis)
+        
+    # Plot fp16 values in interval
+    c = np.float16(-65.0)
+    while c < -64.5:
+        initial_axis.axhline(c, linestyle=":", alpha=0.2, color="gray")
+        c = np.nextafter(c, -64)
+
+    # Zoom in on different bits
+    spike_axis.set_xlim((13.25, 13.85))
+    spike_axis.set_ylim((-50.25, -50.0))
+    
+    initial_axis.set_xlim((0.0, 0.4))
+    initial_axis.set_ylim((-65.0, -64.6))
+    
+    
 fig = plt.figure(frameon=False, figsize=(plot_settings.double_column_width, 2.0))
 
 # Create outer gridspec with four columns
-gsp = gs.GridSpec(1, 3)
+gsp = gs.GridSpec(1, 4, width_ratios=[0.25, 0.25, 0.5 / 3, 1.0 / 3])
+
 
 # Create two sub-gridspecs to divide these columns into gridspecs for voltage traces and box plots
-boxplot_gsp = gs.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsp[2], hspace=0.3)
+boxplot_gsp = gs.GridSpecFromSubplotSpec(2, 2, subplot_spec=gsp[3], hspace=0.4)
+inset_gsp = gs.GridSpecFromSubplotSpec(2, 1, subplot_spec=gsp[2], hspace=0.4)
 
 # Create axes within voltage gridspec
 low_rate_v_axis = plt.Subplot(fig, gsp[0])
 high_rate_v_axis = plt.Subplot(fig, gsp[1])
+
+# Create axes for insets
+spike_v_axis = plt.Subplot(fig, inset_gsp[0,0])
+start_v_axis = plt.Subplot(fig, inset_gsp[1,0])
 
 # Create axes within violin plot gridspec
 low_acc_lag_axis = plt.Subplot(fig, boxplot_gsp[0,0])
@@ -109,6 +148,9 @@ high_rmse_lag_axis = plt.Subplot(fig, boxplot_gsp[1,1])
 # Add axes
 fig.add_subplot(low_rate_v_axis)
 fig.add_subplot(high_rate_v_axis, sharey=low_rate_v_axis)
+fig.add_subplot(spike_v_axis)
+fig.add_subplot(start_v_axis)
+
 fig.add_subplot(low_acc_lag_axis)
 fig.add_subplot(low_rmse_lag_axis, sharex=low_acc_lag_axis)
 fig.add_subplot(high_acc_lag_axis, sharey=low_acc_lag_axis)
@@ -119,17 +161,22 @@ plot_voltage(high_rate_v_axis, 4000.0)
 low_rate_v_axis.set_xlim((4800.0, 5000.0))
 high_rate_v_axis.set_xlim((1800.0, 2000.0))
 
+plot_divergence(spike_v_axis, start_v_axis, 4000.0, 0, 
+                half_actor.get_color(), half_rescale_actor.get_color())
+
 plot_lag(low_acc_lag_axis, 16000.0, half_actor.get_color(), half_rescale_actor.get_color())
 plot_lag(high_acc_lag_axis, 4000.0, half_actor.get_color(), half_rescale_actor.get_color())
 plot_rmse(low_rmse_lag_axis, 16000.0, half_actor.get_color(), half_rescale_actor.get_color())
 plot_rmse(high_rmse_lag_axis, 4000.0, half_actor.get_color(), half_rescale_actor.get_color())
 
-low_rate_v_axis.set_title("A", loc="left")
-high_rate_v_axis.set_title("B", loc="left")
-low_acc_lag_axis.set_title("C", loc="left")
-low_rmse_lag_axis.set_title("E", loc="left")
-high_acc_lag_axis.set_title("D", loc="left")
-high_rmse_lag_axis.set_title("F", loc="left")
+low_rate_v_axis.set_title("A", x=-0.1333)
+high_rate_v_axis.set_title("B", x=-0.1333)
+spike_v_axis.set_title("C", x=-0.2)
+start_v_axis.set_title("D", x=-0.2)
+low_acc_lag_axis.set_title("E", x=-0.2)
+low_rmse_lag_axis.set_title("G", x=-0.2)
+high_acc_lag_axis.set_title("F", x=-0.2)
+high_rmse_lag_axis.set_title("H", x=-0.2)
 
 # Label axes
 low_acc_lag_axis.set_ylabel("Lag [s]")
